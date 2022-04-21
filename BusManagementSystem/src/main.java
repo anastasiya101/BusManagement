dfe6af18142e74f14b15ba2226572675f425cb50
Anastasiya <94914136+anastasiya101@users.noreply.github.com>
2022-04-21 12:45:27 +0100

    added user interface

--- BusManagementSystem/src/main.java
+++ BusManagementSystem/src/main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("hi");
+
+        boolean exitProgram = false;
+        while (!exitProgram) {
+            System.out.println("Enter 1 to search for the shortest path between two bus stops of your choosing. \n" +
+                    "Enter 2 to search for a certain bus stop by name/prefix. \n" +
+                    "Enter 3 to search by arrival time. \n" +
+                    "Or type 'exit' to terminate the program.\n");
+            System.out.println("Please enter 1, 2, 3 or type 'exit'");
+
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) {
+                int input = scanner.nextInt();
+                switch(input) {
+                    case 1:
+                        //findShortestPath();
+                        break;
+                    case 2:
+                        //searchBusStopName();
+                        break;
+                    case 3:
+                        //searchArrivalTime();
+                        break;
+                    default:
+                        System.out.println("Please enter a valid input value of the following integers 1, 2, 3 or type 'exit'.\n");
+                    }
+            } else {
+                String input = scanner.next();
+                if (input.equalsIgnoreCase("exit")) {
+                    exitProgram = true;
+                    System.out.println("The program has now terminated.");
+                } else {
+                    System.out.println("Please enter a valid input value of the following integers 1, 2, 3 or type 'exit'.\n");
+                }
+            }
+        }
+
     }
 }
