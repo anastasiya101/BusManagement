95f3ae00eb39102e276fb318f23fb3aa8ba1f5cf
Anastasiya <94914136+anastasiya101@users.noreply.github.com>
2022-04-22 09:21:37 +0100

    added search bus stop name function

--- BusManagementSystem/src/main.java
+++ BusManagementSystem/src/main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("hi");
     }
+
+    public static void findShortestPath() {
+
+    }
+
+    public static void searchBusStopName() {
+        System.out.println("Please enter a stops full name or the first few characters of the bus stop you are searching for: ");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNext()){
+            String input = scanner.next();
+            int count = 0;
+            for (String string : TST.keysWithPrefix(input.toUpperCase())) {
+                count++;
+                System.out.println(string);
+            }
+            if (count == 0) {
+                System.out.println("Stop name not found, please try again.");
+            }
+        }
+    }
+
+    public static void searchArrivalTime() {
+
+    }
+
 }
